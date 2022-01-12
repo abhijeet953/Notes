a030c4a9a0a66040b254768578a412c4218a305f
Abhijeet <80038035+abhijeet953@users.noreply.github.com>
2022-01-12 15:20:38 +0530

    Add files via upload

---
+++ script/notes-function.js
@@ -0,0 +1,136 @@
+//get your notes
+const getSavedNotes=() =>
+{ 
+    const notesJSON=localStorage.getItem('notes')
+    try
+    {
+      if(notesJSON!==null)
+      {
+      return JSON.parse(notesJSON)
+      }
+      else
+      {
+         return []
+      } 
+   }
+   catch(e)
+   {
+      return []
+   }
+}
+//save notes to the local storage
+const savedNotes= (notes) =>  
+{
+   localStorage.setItem('notes',JSON.stringify(notes))
+}
+//remove note
+const removeNote= (id) =>
+{
+   const noteIndex=notes.findIndex( (note) => note.id===id)
+   if(noteIndex>-1)
+   notes.splice(noteIndex,1);
+
+}
+//generate dom
+const generateNoteDOM= (note) =>
+{
+      const noteEl=document.createElement('a');
+      const textEl=document.createElement('p');
+      const statusEl=document.createElement('p');
+      //Setup the note title text
+      if(note.title.length > 0)
+      {
+         textEl.textContent=note.title
+      }
+      else
+      {
+         textEl.textContent='Unnamed Note'
+      }
+      textEl.classList.add("list-item__title")
+      noteEl.appendChild(textEl)
+
+      noteEl.setAttribute('href',`/edit.html#${note.id}`)
+      noteEl.classList.add('list-item')
+
+      statusEl.textContent=generateLastEdited(note.updatedAt)
+      statusEl.classList.add('list-item__subtitle')
+      
+      noteEl.appendChild(statusEl)
+
+      return noteEl
+}
+
+//Sort your Notes
+const sortNotes= (notes,sortBy) =>
+{
+   if(sortBy === 'byEdited')
+   return notes.sort((a,b) =>
+   {
+      if(a.updatedAt > b.updatedAt)
+      return -1
+      else if(a.updatedAt < b.updatedAt)
+      return 1
+      else 
+      return 0
+   })
+   else if(sortBy === 'byCreated')
+   {
+      return notes.sort((a,b) =>
+      {
+         if(a.createdAt > b.createdAt)
+         return -1
+         else if (a.createdAt < b.createdAt)
+         return 1
+         else 
+         return 0
+      })
+   }
+   else if(sortBy === 'alphabetical')
+   {
+      return notes.sort((a,b) =>
+      {
+         if(a.title.toLowerCase() < b.title.toLowerCase())
+         return -1
+         else if(a.title.toLowerCase() > b.title.toLowerCase())
+         return 1
+         else 
+         return 0
+      })
+   }
+   else 
+   return notes
+}
+
+//render
+const renderNotes=(notes,filters) =>
+{
+   let notesEl=document.querySelector('#notes')
+   notes=sortNotes(notes,filters.sortBy)
+   const filteredNotes=notes.filter((note)=>note.title.toLowerCase().includes(filters.searchText.toLowerCase()))
+   // console.log(filteredNotes);
+      notesEl.innerHTML = ''
+
+   if(filteredNotes.length>0)
+   {
+      filteredNotes.forEach((note)=>
+   {
+      const noteEl=generateNoteDOM(note)
+      notesEl.appendChild(noteEl)
+   })
+   }
+   else
+   {
+      const emptyMessage=document.createElement('p')
+      emptyMessage.textContent='No notes to show'
+      emptyMessage.classList.add('empty-message')
+      notesEl.appendChild(emptyMessage)
+   }
+
+   
+}
+
+//generate the last edited message
+const generateLastEdited=(timestamp)=>
+{
+   return `Last edited ${moment(timestamp).fromNow()}` 
+}
---
+++ script/notes_app.js
@@ -0,0 +1,47 @@
+let notes = getSavedNotes()
+
+const filters={
+   searchText:'',
+   sortBy:'byEdited'
+}
+
+renderNotes(notes,filters)
+
+document.querySelector('#create').addEventListener('click',function(e)
+{
+   const id=uuidv4()
+   const timestamp=moment().valueOf()
+  notes.push({
+     id:id,
+     title:'',
+     body:'',
+     createdAt: timestamp,
+     updatedAt:timestamp
+  })
+  savedNotes(notes)
+//   renderNotes(notes,filters)
+  location.assign(`/edit.html#${id}`)
+})
+
+document.querySelector('#search-text').addEventListener('input',(e)=>
+{
+   filters.searchText=e.target.value
+   renderNotes(notes,filters)
+})
+
+document.querySelector('#filter-by').addEventListener('change',(e)=>
+{
+   filters.sortBy=e.target.value
+   renderNotes(notes,filters)
+})
+
+//syncing data across pages
+window.addEventListener('storage',(e)=>
+{
+   if(e.key === 'notes')
+   {
+      notes=JSON.parse(e.newValue)
+      renderNotes(notes,filters)
+   }
+})
+
